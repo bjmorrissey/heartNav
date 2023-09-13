d5a695bbf5d9aae851e92ee27ed403630a52c00f
Brian Morrissey <brianjmorrissey@gmail.com>
2023-09-13 07:00:53 -0500

    editing animation of heart

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 console.log('here')
 
-const nav = document.getElementsByName('nav')
+const nav = document.querySelector('nav')
+let checked = false
 
-nav.addEventListener('click', () =>{console.log('clicked')})
+function clicked() {
+    
+    if (!checked) {
+        checked = true
+        nav.classList.add('openNav')
+        console.log(checked)
+        console.log('nav checked, should be open')
+    } else {
+        checked = false
+        nav.classList.remove('openNav')
+        console.log(checked)
+        console.log('nav closed, should be false')
+    }
+}
+
+nav.addEventListener('click', clicked)
