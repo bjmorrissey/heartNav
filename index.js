3ee6fa5be8e15b0ca6aa86ce0348e1ac1bc534aa
bjmorrissey <brianjmorrissey@gmail.com>
2023-09-15 11:34:48 -0500

    edits from work

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ console.log('here')
 
 const heart = document.querySelector('.navHeart')
 const greet = document.querySelector('.greet')
+const navigation = document.querySelector('.navigation')
+const brian = document.querySelector('.brian')
 let checked = false
 
 function clicked() {
@@ -10,6 +12,10 @@ function clicked() {
         checked = true
         heart.classList.remove('heartClickOff')
         heart.classList.add('heartClickOn')
+        navigation.classList.remove('navBarRemove')
+        navigation.classList.add('navBarAnimate')
+        brian.classList.remove('disappear')
+        brian.classList.add('appear')
         greet.classList.add('disappear')
         console.log(checked)
         console.log('nav checked, should be open')
@@ -17,7 +23,12 @@ function clicked() {
         checked = false
         heart.classList.remove('heartClickOn')
         heart.classList.add('heartClickOff')
+        navigation.classList.remove('navBarAnimate')
+        navigation.classList.add('navBarRemove')
         greet.classList.remove('disappear')
+        brian.classList.remove('appear')
+        brian.classList.add('disappear')
+        
         console.log(checked)
         console.log('nav closed, should be false')
     }
